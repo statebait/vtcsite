50547bfa7cc8e62d580615b13f2483336de0dc65
Mohamed Shadab <shadabdragon@gmail.com>
2017-12-29 23:54:31 +0530

    Loads of new features and fixes

--- js/admin.js
+++ js/admin.js
@@ -10,6 +10,18 @@ rootRef.on("child_added", snap => {
   $("#table_body").append("<tr><td>" + name + "</td><td>" + contact + "</td><td>" + age + "</td><td>" + sex + "</td><td>" + course + "</td></tr>");
 });
 
+var placeRef = firebase.database().ref().child('detailsforms');
+
+placeRef.on("child_added", snap => {
+  var name = snap.child('name').val();
+  var contact = snap.child('contact').val();
+  var certificate = snap.child('certificate').val();
+  var company = snap.child('company').val();
+  var jobno = snap.child('jobno').val();
+
+  $("#table_body2").append("<tr><td>" + name + "</td><td>" + contact + "</td><td>" + certificate + "</td><td>" + company + "</td><td>" + jobno + "</td></tr>");
+});
+
 //Reference form data collection
 var messageRef = firebase.database().ref('placementlisting');
 
@@ -23,9 +35,10 @@ function clicky(e) {
   var company = getInputVal('company-name');
   var type = getInputVal('worker-type');
   var number = getInputVal('worker-no');
+  var wages = getInputVal('wages');
 
   //Save Message
-  saveMessage(company, type, number);
+  saveMessage(company, type, number, wages);
   /*
     //Show alert
     document.querySelector('.alert').style.display = 'block';
@@ -37,17 +50,53 @@ function clicky(e) {
 }
 
 //function to get value from form
-
 function getInputVal(id) {
   return document.getElementById(id).value;
 }
 
 //saving form to firebase
-function saveMessage(company, type, number) {
+function saveMessage(company, type, number, wages) {
   var newMessageRef = messageRef.push();
   newMessageRef.set({
     company: company,
     type: type,
     number: number,
+    wages: wages
   });
+}
+
+function show_enroll() {
+  var x = document.getElementById("enrollment_table");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function show_enroll2() {
+  var x = document.getElementById("placed_table");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function delete_dataset() {
+
+  var table = getInputVal('table_select');
+  var uid = getInputVal('data_id');
+  console.log(uid);
+  console.log(table);
+  if (table == "Placement Company") {
+    var deleteRef = firebase.database().ref('placementlisting').child(uid);
+    deleteRef.remove();
+  } else if (table == "Enrollment Forms") {
+    var deleteRef = firebase.database().ref('enrollforms').child(uid);
+    deleteRef.remove();
+  } else if (table == "Placed Workers") {
+    var deleteRef = firebase.database().ref('detailsforms').child(uid);
+    deleteRef.remove();
+  }
 }
--- js/placement.js
+++ js/placement.js
@@ -4,7 +4,47 @@ rootRef.on("child_added", snap => {
   var company = snap.child('company').val();
   var number = snap.child('number').val();
   var type = snap.child('type').val();
+  var wages = snap.child('wages').val();
+  var jobno = snap.key;
 
-  $("#table_body").append("<tr><td>" + company + "</td><td>" + type + "</td><td>" + number + "</td><td><button class=btn>Enroll</button></td></tr>");
+  $("#table_body").append("<tr><td>" + company + "</td><td>" + type + "</td><td>" + number + "</td><td>" + wages + "</td><td>" + jobno + "</td></tr>");
 
 });
+
+//Reference form data collection
+var messageRef = firebase.database().ref('detailsforms');
+
+//Listen for Submit
+document.getElementById('detailsform').addEventListener('submit', clicky);
+
+function clicky(e) {
+  e.preventDefault();
+
+  //Get Values
+  var name = getInputVal('worker_name');
+  var contact = getInputVal('contact_no');
+  var certificate = getInputVal('certi_no');
+  var company = getInputVal('company_name')
+  var jobno = getInputVal('jobno');
+
+  //Save Message
+  saveMessage(name, contact, certificate, company, jobno);
+}
+
+//function to get value from form
+
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+//saving form to firebase
+function saveMessage(name, contact, certificate, company, jobno) {
+  var newMessageRef = messageRef.push();
+  newMessageRef.set({
+    name: name,
+    contact: contact,
+    certificate: certificate,
+    company: company,
+    jobno: jobno,
+  });
+}
